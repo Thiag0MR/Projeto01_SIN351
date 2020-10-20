e3542260702a5188ddd740e0e7fdeace9474c3a4
Gabriel Alves <gabrielalvesresenderocha@gmail.com>
2020-10-20 15:53:52 -0300

    Matriz

--- meuShell.c
+++ meuShell.c
@@ -6,7 +6,8 @@
 
 void pegar_entrada(char* linha) {
     char c;
-    for (int i = 0; i < 512; i++) {
+    int i =0;
+    for ( i ; i < 512; i++) {
         scanf("%c", &c);
         if (c == '\n') {
             linha[i] = '\0';
@@ -22,31 +23,43 @@ int contar_argumentos (linha) {
 
 }
 
-void analisa_entrada (char* linha, char* comandos) {
+void analisa_entrada (char* linha, char comandos[512][512]) {
 
-    comandos[0][0] = linha[0];
-
+	int i = 1;
     int k = 0;
-
-    for (int i = 1; i < 512; i++) {
-        char c = linha[i-1];
-
+    int final = 0;
+    int comeco = 0;
+	int col = 0;
+    int lin = 0;
+    char c = 0;
+    int espaco = 0;
+    
+    for ( i ; i < 512; i++) {
+        c = linha[i-1];
+		final = i;
         if (c == ' ') {
+        	espaco++;
             if (linha[i] != ' ') {
-                j++;
-                comandos[j][i] = linha[i];
-            }
-        } else {
-            comandos[j][i] = linha[i];
-        }
+            	espaco ++;
+            	final -= (espaco + 1);
+            	
+            	
+            	for(col ; col < 10;){
+            		for(lin ; lin < final; lin++){
+            			comandos[col][lin]= linha[comeco];
+            			comeco++;
+					}
+				}
+				col++;
+				comeco += (final + (espaco + 1));
+				
+                
+            }else if (linha[i] == '\0'){
+            	break;
+			}
+    	} 
+	}
 
-        if (linha[i] == '\0') {
-            break;
-        }
-    }
-}
-
-ls -la
 
 int main(int argc, char const *argv[]) {
 
