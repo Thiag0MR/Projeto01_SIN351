1625b5b566fa7f273eb2ee8386cc2f252e873e08
thiagoomendes2010@gmail.com <thiagoomendes2010@gmail.com>
2020-10-20 14:48:14 -0300

    teste

--- meuShell.c
+++ meuShell.c
@@ -18,38 +18,46 @@ void pegar_entrada(char* linha) {
 }
 
 int contar_argumentos (linha) {
-    int contador = 0;
+
+
+}
+
+void analisa_entrada (char* linha, char* comandos) {
+
+    comandos[0][0] = linha[0];
+
+    int k = 0;
+
     for (int i = 1; i < 512; i++) {
         char c = linha[i-1];
 
         if (c == ' ') {
             if (linha[i] != ' ') {
-                contador++;
+                j++;
+                comandos[j][i] = linha[i];
             }
+        } else {
+            comandos[j][i] = linha[i];
         }
 
         if (linha[i] == '\0') {
             break;
         }
     }
-
-    return contador;
-}
-char* analisa_entrada (char* linha) {
-
 }
 
+ls -la
 
 int main(int argc, char const *argv[]) {
 
     char linha[512];
-
-    ls -la
+    char comandos[512][512];
 
     while (1) {
         printf("meuShell@meuShell-pc:$ > ");
         pegar_entrada(linha);
-        // analisa_entrada(linha);
+        analisa_entrada(linha, comandos);
+
 
         printf("%s\n", linha);
 
