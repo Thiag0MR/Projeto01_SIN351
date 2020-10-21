ce41b02ba674c736d0d081f4d700ad90768d6794
thiagoomendes2010@gmail.com <thiagoomendes2010@gmail.com>
2020-10-21 13:08:21 -0300

    troquei nome função para split_entrada

--- meuShell.c
+++ meuShell.c
@@ -24,7 +24,7 @@ void split_entrada (char* linha, char comandos[][512]) {
     int l = 0, inicio = 0, fim = 0;
     bool espaco = false;
 
-<<<<<<< HEAD
+
     for (int i = 0; i < 512; i++) {
         if (i == 0) {
             inicio = i;
@@ -77,10 +77,7 @@ void funcao_imprimir_comandos (char comandos[][512]) {
 }
 
 
-=======
-}
-
-void analisa_entrada (char* linha, char comandos[512][512]) {
+void analisa_entrada (char* linha, char comandos[][512]) {
 
 	int i = 1;
     int k = 0;
@@ -90,7 +87,7 @@ void analisa_entrada (char* linha, char comandos[512][512]) {
     int lin = 0;
     char c = 0;
     int espaco = 0;
-    
+
     for ( i ; i < 512; i++) {
         c = linha[i-1];
 		final = i;
@@ -99,25 +96,25 @@ void analisa_entrada (char* linha, char comandos[512][512]) {
             if (linha[i] != ' ') {
             	espaco ++;
             	final -= (espaco + 1);
-            	
-            	
-            	for(col ; col < 10;){
-            		for(lin ; lin < final; lin++){
+
+
+            	for (col ; col < 10;) {
+            		for (lin ; lin < final; lin++) {
             			comandos[col][lin]= linha[comeco];
             			comeco++;
 					}
 				}
 				col++;
 				comeco += (final + (espaco + 1));
-				
-                
-            }else if (linha[i] == '\0'){
+
+
+            } else if (linha[i] == '\0'){
             	break;
 			}
-    	} 
+    	}
 	}
+}
 
->>>>>>> e3542260702a5188ddd740e0e7fdeace9474c3a4
 
 int main(int argc, char const *argv[]) {
 
@@ -129,6 +126,7 @@ int main(int argc, char const *argv[]) {
         pegar_entrada(linha);
 
         split_entrada(linha, comandos);
+        // analisa_entrada(linha, comandos);
 
         funcao_imprimir_comandos(comandos);
 
